773296ee7f49ea62e8cb2bf1abaebdc2ddb60a36
Bartosz Meglicki <bmegli@op.pl>
2019-02-21 23:28:00 +0100

    MPL 2.0 license headers
    apart from repository level notice

--- Assets/RawImageVideoRenderer.cs
+++ Assets/RawImageVideoRenderer.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿/*
+ * Unity Network Hardware Video Decoder
+ * 
+ * Copyright 2019 (C) Bartosz Meglicki <meglickib@gmail.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+using System;
 using UnityEngine;
 using UnityEngine.UI; //RawImage
 
